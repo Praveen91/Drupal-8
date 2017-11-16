18ca10f34e762cc6462b9c3b964b1d29ea2fbb75
praveen_91 <praveen_91@3374490.no-reply.drupal.org>
2017-11-17 00:06:13 +0530

    upgrade module with CURD

--- resume/src/Form/ResumeForm.php
+++ resume/src/Form/ResumeForm.php
@@ -19,7 +19,7 @@ class ResumeForm extends FormBase {
      ];
      $form['candidate_email'] = [
        '#title' => $this->t('Email'),
-       '#type' => 'eamil',
+       '#type' => 'email',
        '#required' => TRUE,
      ];
      $form['candidate_phone'] = [
@@ -61,7 +61,7 @@ class ResumeForm extends FormBase {
       '#value' => $this->t('Save'),
       '#button_type' => 'primary',
     ];
-    
+
      return $form;
   }
 
@@ -69,12 +69,52 @@ class ResumeForm extends FormBase {
       if (strlen($form_state->getValue('candidate_phone')) < 10 ) {
       	$form_state->setErrorByName('candidate_phone', $this->t('Mobile No is too shrot'));
       }
+
+      $name = $form_state->getValue('candidate_name');
+      if(preg_match('/[^A-Za-z]/', $name)) {
+         $form_state->setErrorByName('candidate_name', $this->t('your name must in characters without space'));
+      }
+
+      if (!intval($form_state->getValue('candidate_phone'))) {
+             $form_state->setErrorByName('candidate_age', $this->t('Age needs to be a number'));
+      }
+    //parent::validateForm($form, $form_state);
   }
 
   public function submitForm(array &$form, FormStateInterface $form_state) {
-	 foreach ($form_state->getValues() as $key => $value) {
+	 /*foreach ($form_state->getValues() as $key => $value) {
 	      drupal_set_message($key . ': ' . $value);
-	    }
+	    }*/
+     $account = \Drupal::currentUser();
+     $fields = $form_state->getValues();
+     $name = $fields['candidate_name'];
+     //echo $account->id(); exit();  //Praveen
+
+     $mail = $fields['candidate_email'];
+     $mobile = $fields['candidate_phone'];
+     $dob = strtotime($fields['candidate_dob']);
+     $gender = $fields['candidate_gender'];
+     $confirm = $fields['candidate_confirmation'];
+     $copy = $fields['candidate_copy'];
+//echo time(); exit();
+     $field = [
+      'uid' => $account->id(),
+      'candidate_name' => $name,
+      'candidate_email' => $mail,
+      'candidate_phone' => $mobile,
+      'candidate_dob' => $dob,
+      'candidate_gender' => $gender,
+      'candidate_confirmation' => $confirm,
+      'candidate_copy' => $copy,
+      'created' => time(),
+     ];
+
+     $query = \Drupal::database();
+     $query->insert('candidate_resume')
+           ->fields($field)
+           ->execute();
+     drupal_set_message('Candidate Resume Data has been saved.');
+
   }
 
 }
